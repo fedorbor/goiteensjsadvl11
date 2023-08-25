81d3a76b76642d3ba204c01204d089653c522a14
fedorbor <112242236+fedorbor@users.noreply.github.com>
2023-08-25 16:23:06 +0300

    dz

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
+// Функція, що повертає проміс з затримкою
+function delayedPromise(value, delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value);
+    }, delay);
+  });
+}
 
-
-const horses = [
-  'Secretariat',
-  'Eclipse',
-  'West Australian',
-  'Flying Fox',
-  'Seabiscuit',
+// Створення масиву з 5 промісів
+const promisesArray1 = [
+  delayedPromise('Проміс 1', 2000),
+  delayedPromise('Проміс 2', 1000),
+  delayedPromise('Проміс 3', 3000),
+  delayedPromise('Проміс 4', 1500),
+  delayedPromise('Проміс 5', 2500),
 ];
 
-const refs = {
-  startBtn: document.querySelector('.js-start-race'),
-  winnerField: document.querySelector('.js-winner'),
-  progressField: document.querySelector('.js-progress'),
-  tableBody: document.querySelector('.js-results-table > tbody'),
-};
+// Виконання всіх промісів одночасно та обробка результатів
+Promise.all(promisesArray1)
+  .then((results) => {
+    console.log('Результати вирішення промісів:', results);
+  })
+  .catch((error) => {
+    console.error('Сталася помилка:', error);
+  });
 
-
-
-
-  function run (horse){
-    const time = getRandomTime(1500, 2500);
-    return new Promise(resolve=>{
-      setTimeout(
-        ()=>{
-        resolve({horse, time})
-      },
-      time)
-    })
-  }
-let x = 0
-function getRandomTime(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  // Функція, що повертає проміс з випадковою затримкою
+function randomDelay(value) {
+  const randomTimeout = Math.random() * (5000 - 1000) + 1000; // Випадкова затримка від 1000 до 5000 мілісекунд
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value);
+    }, randomTimeout);
+  });
 }
-refs.startBtn.addEventListener('click', () =>{
-    x++
-   refs.progressField.innerHTML = 'Заїзд почався';
-    const promiseArr = horses.map(horse => run(horse))
-    Promise.race(promiseArr).then(({horse,time}) => {
-        refs.winnerField.textContent = `🎉 Переможець ${horse}, финишував за ${time}мс часу`
-      const tr = `<tr><td>${x}</td><td>${horse}</td><td>${time}</td></tr>`
-      refs.tableBody.insertAdjacentHTML("beforeend",tr) 
-    })
-    Promise.all(promiseArr).then(()=> {
-        refs.progressField.innerHTML = 'Заїзд закінчився'
-    })
-})
+
+// Створення масиву з 5 промісів
+const promisesArray2 = [
+  randomDelay('Проміс 1'),
+  randomDelay('Проміс 2'),
+  randomDelay('Проміс 3'),
+  randomDelay('Проміс 4'),
+  randomDelay('Проміс 5'),
+];
+
+// Виконання всіх промісів одночасно та отримання результату найшвидшого проміса
+Promise.race(promisesArray2)
+  .then((fastestResult) => {
+    console.log('Найшвидший проміс:', fastestResult);
+  })
+  .catch((error) => {
+    console.error('Сталася помилка:', error);
+  });
